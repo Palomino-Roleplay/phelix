889df205e62c46d61a8e9ade89c65b46d452c53e
sil <git@sil.dev>
2023-07-25 20:57:06 -0500

    Reworked attachments, now work w/ inventory item dragging system

--- gamemode/items/base/sh_attachments.lua
+++ gamemode/items/base/sh_attachments.lua
@@ -4,18 +4,20 @@ ITEM.model = "models/Items/BoxSRounds.mdl"
 ITEM.width = 1
 ITEM.height = 1
 ITEM.attachment = "md_rugersup"
+ITEM.weapons = {}
+ITEM.isAttachment = true
 ITEM.description = "A box that contains a weapon attachment."
 ITEM.category = "Attachments"
 ITEM.useSound = "items/ammo_pickup.wav"
 
 -- On player uneqipped the item, Removes a weapon from the player and keep the ammo in the item.
-ITEM.functions.use = {
-	name = "Attach",
-	tip = "useTip",
-	icon = "icon16/add.png",
-	OnRun = function(item)
-        CustomizableWeaponry:giveAttachment( item.player, item.attachment )
+-- ITEM.functions.use = {
+-- 	name = "Attach",
+-- 	tip = "useTip",
+-- 	icon = "icon16/add.png",
+-- 	OnRun = function(item)
+--         CustomizableWeaponry:giveAttachment( item.player, item.attachment )
 
-		return true
-	end,
-}
+-- 		return true
+-- 	end,
+-- }
--- gamemode/items/base/sh_weapons.lua
+++ gamemode/items/base/sh_weapons.lua
@@ -73,6 +73,22 @@ ITEM:Hook("drop", function(item)
 	end
 end)
 
+ITEM.functions.combine = {
+	OnRun = function( item, data )
+		Print("OnRun")
+		local sAttachment = ix.item.instances[data[1]].attachment
+		ix.item.instances[data[1]]:Remove()
+
+		item:SetData("attachments", table.Add(item:GetData("attachments", {}), {sAttachment}))
+
+		return false
+	end,
+	OnCanRun = function( item, data )
+		Print("OnCanRun")
+		return ix.item.instances[data[1]].isAttachment and ix.item.instances[data[1]].weapons[item.class]
+	end
+}
+
 -- On player uneqipped the item, Removes a weapon from the player and keep the ammo in the item.
 ITEM.functions.EquipUn = { -- sorry, for name order.
 	name = "Unequip",
@@ -172,6 +188,16 @@ function ITEM:Equip(client, bNoSelect, bNoSound)
 			client:SetAmmo(1, ammoType)
 		end
 
+		if self:GetData( "attachments", {} ) then
+			for _, sAttachment in pairs( self:GetData( "attachments", {} ) ) do
+				CustomizableWeaponry:giveAttachment( client, sAttachment )
+
+				timer.Simple( 0.1, function() -- hacky fix for attachments not being applied
+					weapon:attachSpecificAttachment( sAttachment )
+				end )
+			end
+		end
+
 		self:SetData("equip", true)
 
 		if (self.isGrenade) then
@@ -207,6 +233,12 @@ function ITEM:Unequip(client, bPlaySound, bRemoveItem)
 		client:StripWeapon(self.class)
 	else
 		print(Format("[Helix] Cannot unequip weapon - %s does not exist!", self.class))
+	end
+
+	if self:GetData( "attachments", {} ) then
+		for _, sAttachment in pairs( self:GetData("attachments", {} ) ) do
+			CustomizableWeaponry:removeAttachment( client, sAttachment )
+		end
 	end
 
 	if (bPlaySound) then
@@ -253,6 +285,16 @@ function ITEM:OnLoadout()
 
 			weapon.ixItem = self
 			weapon:SetClip1(self:GetData("ammo", 0))
+
+			if self:GetData( "attachments", {} ) then
+				for _, sAttachment in pairs( self:GetData( "attachments", {} ) ) do
+					CustomizableWeaponry:giveAttachment( client, sAttachment )
+	
+					timer.Simple( 0.1, function() -- hacky fix for attachments not being applied
+						weapon:attachSpecificAttachment( sAttachment )
+					end )
+				end
+			end
 
 			if (self.OnEquipWeapon) then
 				self:OnEquipWeapon(client, weapon)
